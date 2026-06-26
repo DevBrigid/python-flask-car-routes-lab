97491cc60ad54232faa849f59eccf41e12fc4260
DevBrigid <mukamibrigid20@gmail.com>
2026-06-26 11:08:53 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to Flatiron Cars"
+
+@app.route("/<model>")
+def get_model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
